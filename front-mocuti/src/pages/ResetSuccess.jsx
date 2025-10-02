bb068abe2b22e56919356503f46c43117c7ea0af
kevellyoliveira <kevelly.oliveira@sptech.school>
2025-10-02 08:37:29 -0300

    Criando rotas

---
+++ front-mocuti/src/pages/ResetSuccess.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function ResetSuccess() {
+  return (
+    <div>
+      <h2>Senha alterada com sucesso 🎉</h2>
+      <a href="/login">Voltar ao login</a>
+    </div>
+  );
+}
