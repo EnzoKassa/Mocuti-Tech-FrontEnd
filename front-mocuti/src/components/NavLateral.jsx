61aa7720697148c3892766d85d3b0556fb934d9f
EnzoKassa <enzo.kassawara@sptech.school>
2025-10-06 20:04:20 -0300

    Inicio de desenvolvimento Feedback Dash M1

--- front-mocuti/src/components/NavLateral.jsx
+++ front-mocuti/src/components/NavLateral.jsx
@@ -4,8 +4,9 @@ import { BotaoNav } from "./BotaoNav";
 import Person from "../assets/images/Person.svg"
 import Calendario from '../assets/images/calendario.svg'
 import ListaUser from "../assets/images/icone Lista Usuarios.svg"
-import MeuPerfil from "../assets/images/meuPerfil.svg"
 import VisaoGeral from "../assets/images/visaoGeral.svg"
+import Calendario2 from "../assets/images/calendario2.svg"
+import MeuPerfil from "../assets/images/meuPerfil.svg"
 import Sair from "../assets/images/sair.svg";
 
 export function NavLateral() {
@@ -21,9 +22,9 @@ export function NavLateral() {
       <div className="boxLinksNav">
         <BotaoNav texto="Eventos" imgLink={Calendario} />
         <BotaoNav texto="Lista de Usuários" imgLink={ListaUser} />
-        <BotaoNav texto="Meu Perfil" imgLink={MeuPerfil} />
+        <BotaoNav texto="Feedbacks" imgLink={Calendario2} />
+        <BotaoNav texto="Meu Perfil" imgLink={MeuPerfil} />  
         <BotaoNav texto="Visão Geral" imgLink={VisaoGeral} />
-        
       </div>
 
       {/* Botão sair */}
--- front-mocuti/src/pages/M1/feedbacks_M1.jsx
+++ front-mocuti/src/pages/M1/feedbacks_M1.jsx
@@ -1,7 +1,115 @@
-import React, { useEffect, useState } from "react";
+import { NavLateral } from "../../components/NavLateral";
+import '../../styles/TelaComNavLateral.css';
+import '../../styles/Feedbacks_M1.css';
 
-const FeedbacksM1 = () => {
-  return <h1>Feedbacks m1</h1>;
-};
+function Feedbacks_M1() {
+  return (
+    <>
+      <div className="TelaComNavLateral">
 
-export default FeedbacksM1;
+        <NavLateral />
+
+        <div className="MainContent">
+
+          <div className="boxTopFeedback">
+
+            <div className="tituloFeedback">Feedback Dashboard </div>
+
+            <div className="boxDashboardFeedback">
+
+              <div className="dashPalestra">
+
+              </div>
+
+              <div className="dashQuantidadeFeedback">
+
+              </div>
+
+            </div>
+
+          </div>
+
+
+          <div className="boxLowFeedback">
+
+
+            <div className="tituloFeedback"> Lista de Feedbacks </div>
+
+
+            <div className="boxListaFeedback">
+
+
+              <div className="colunas">
+
+
+
+                <div className="colunaFeedback">
+
+                </div>
+
+
+
+                <div className="colunaNome">
+
+
+
+                </div>
+
+
+                <div className="colunaEmail">
+
+
+
+                </div>
+
+
+
+              </div>
+
+
+              <div className="linhas">
+
+
+
+                <div className="colunaFeedback">
+
+                </div>
+
+
+
+                <div className="colunaNome">
+
+
+
+                </div>
+
+
+                <div className="colunaEmail">
+
+
+
+                </div>
+
+                <div className="boxBotaoFeedback">
+
+                  <button className="botaoMaisInfoFeedback"> Mais Informações </button>
+
+                </div>
+
+              </div>
+
+
+
+            </div>
+
+
+          </div>
+
+
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Feedbacks_M1;
